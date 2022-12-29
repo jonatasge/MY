163fde5a6169cfb130481602f32acbc63e0dd84d
Jonatas Evangelista <jonatasge@gmail.com>
2022-12-29 02:56:21 -0300

    list-chapter

--- scripts/list-chapters.js
+++ scripts/list-chapters.js
@@ -1,15 +1,21 @@
 const listChapters = () => {
   const spans = $('.manga-index-header > span');
-  (spans?.length > 1 ? spans : [spans]).forEach((span) => {
-    const a = $(
-      span.parentElement.parentElement,
-      '.manga-index-content > a'
-    ).cloneNode(true);
 
-    const div = a.firstChild;
-    while (div.childNodes.length > 1) div.removeChild(div.lastChild);
+  (spans?.length > 1 ? spans : [spans]).forEach((span, i) => {
+    let as = $(span.parentElement.parentElement, '.manga-index-content > a');
+    if (!Array.isArray(as)) as = [as];
 
-    span.parentNode.replaceChild(a, span);
+    const linksWrap = document.createElement('div');
+    linksWrap.classList.add('links-wrap');
+
+    as.forEach((_a) => {
+      const a = _a.cloneNode(true);
+      const div = a.firstChild;
+      while (div.childNodes.length > 1) div.removeChild(div.lastChild);
+      linksWrap.appendChild(a);
+    });
+
+    span.parentNode.replaceChild(linksWrap, span);
   });
 };
 
@@ -17,6 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
   Observer({
     target: $('#chapters-list'),
     config: { childList: true },
-    callback: debounce(listChapters, 100),
+    callback: debounce(listChapters),
   });
 });

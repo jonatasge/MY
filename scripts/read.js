50b05715bc71200335151f7cb489ddab5cf06230
Jonatas Evangelista <jonatasge@gmail.com>
2022-12-29 01:54:55 -0300

    read

--- scripts/read.js
+++ scripts/read.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  $('.section.table-of-contents img').forEach(removeListeners);
-  $('.chapter-links a[').forEach(removeListeners);
-});
-
 const removeListeners = (element) => {
   const copy = element.cloneNode(true);
   element.parentNode.replaceChild(copy, element);
 };
+
+$('img').forEach(removeListeners);
+$('.chapter-links a').forEach((a) => {
+  if (!a.id) removeListeners(a);
+});

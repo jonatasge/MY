47266e1d00b167d30fd64fc12ff9648d5856052e
Jonatas Evangelista <jonatasge@gmail.com>
2022-12-29 00:56:26 -0300

    remove local host value

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-const host = 'http://127.0.0.1:8080' || 'https://jonatasgev.github.io/MY';
+const host = 'https://jonatasgev.github.io/MY';
 
 const styles = ['ads', 'nav', 'settings', 'list', 'read'];
 const scripts = ['ads', 'nav', 'read'];
